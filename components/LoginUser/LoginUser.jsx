62c255061cfb52698a517e4bfa7e6cbc7dfe7f15
Ruben <rub.somuano.g2@gmail.com>
2022-05-04 01:56:23 -0500

    singin and login

--- components/LoginUser/LoginUser.jsx
+++ components/LoginUser/LoginUser.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./loginUser.module.scss";
+import { useRouter } from "next/router";
+
+const initialState = {
+  email: "",
+  password: "",
+};
 
 export default function LoginUser() {
+  const router = useRouter();
+
+  const [dataLogin, setDataLogin] = useState(initialState);
+
+  function saveState(event) {
+    // initialState[event.target.name] = event.target.value;
+    setDataLogin({
+      ...dataLogin,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  async function login(event) {
+    event.preventDefault();
+    const url = "http://localhost:8080/organizer/login";
+    const options = {
+      method: "POST",
+      body: JSON.stringify(dataLogin),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    await fetch(url, options)
+      .then((res) => res.json())
+      .then((response) => {
+        console.log(response.data);
+        localStorage.setItem(
+          "tokenUser",
+          JSON.stringify(response.data.token.token)
+        );
+        localStorage.setItem("idUser", JSON.stringify(response.data.token.id));
+        router.push("/dashboard-organizer");
+      });
+  }
+
   return (
     <div className={styles.containerLoginAndImg}>
       <div className={styles.containerLogin}>
@@ -17,21 +58,29 @@ export default function LoginUser() {
                 className={styles.inputLogin}
                 type="text"
                 placeholder="Email"
+                name="email"
+                onChange={saveState}
+                value={dataLogin.email}
               />
             </div>
             <div>
               <p className={styles.upInputLogin}>Password</p>
               <input
                 className={styles.inputLogin}
-                type="text"
+                type="password"
                 placeholder="Password"
+                name="password"
+                onChange={saveState}
+                value={dataLogin.password}
               />
             </div>
             <div className={styles.contetaContaseña}>
               <a className={styles.aContaseña}>Forgot my password</a>
             </div>
           </div>
-          <button className={styles.btnL}>Login</button>
+          <button className={styles.btnL} onClick={login}>
+            Login
+          </button>
         </div>
       </div>
       <div className={styles.contImgLoginW}>
--- components/SignIn/SignIn.jsx
+++ components/SignIn/SignIn.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./signIn.module.scss";
+import { useRouter } from "next/router";
+
+const initialState = {
+  name: "dommi",
+  lastName: "dommi",
+  email: "",
+  password: "",
+  city: "dommi",
+  country: "dommi",
+  telephone: "0000000000",
+};
 
 export default function SignUser() {
+  const router = useRouter();
+
+  const [dataSignIn, setDataSignIn] = useState(initialState);
+
+  function saveState(event) {
+    // initialState[event.target.name] = event.target.value;
+    setDataSignIn({
+      ...dataSignIn,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  async function createUser(event) {
+    event.preventDefault();
+    const url = "https://api.chaan.site/organizer/signup";
+    const options = {
+      method: "POST",
+      body: JSON.stringify(dataSignIn),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    await fetch(url, options)
+      .then((res) => res.json())
+      .then((response) => {
+       /* console.log(response.data.organizer._id);
+        localStorage.setItem(
+          "idUser",
+          JSON.stringify(response.data.organizer._id)
+        );*/
+        router.push("/login-user");
+      });
+  }
+
   return (
     <div className={styles.containerLoginAndImg}>
       <div className={styles.containerLogin}>
@@ -17,18 +62,26 @@ export default function SignUser() {
                 className={styles.inputLogin}
                 type="text"
                 placeholder="Email"
+                name="email"
+                onChange={saveState}
+                value={dataSignIn.email}
               />
             </div>
             <div>
               <p className={styles.upInputLogin}>Password</p>
               <input
                 className={styles.inputLogin}
-                type="text"
+                type="password"
                 placeholder="Password"
+                name="password"
+                onChange={saveState}
+                value={dataSignIn.password}
               />
             </div>
           </div>
-          <button className={styles.btnL}>Sign in</button>
+          <button className={styles.btnL} onClick={createUser}>
+            Sign in
+          </button>
         </div>
       </div>
       <div className={styles.contImgLoginW}>
