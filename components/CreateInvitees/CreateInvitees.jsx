8db60a0414a3f42a6de6bcee1f2a4ce040ce8a6c
Ruben <rub.somuano.g2@gmail.com>
2022-05-10 09:30:33 -0500

    create invitees

---
+++ components/CreateInvitees/CreateInvitees.jsx
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+import styles from "./createInvitees.module.scss";
+import { useRouter } from "next/router";
+
+const initialState = {
+  name: "",
+  lastName: "",
+  email: "",
+  password: "",
+  city: "dommi",
+  country: "dommi",
+  telephone: "0000000000",
+};
+
+export default function createInvitees() {
+  const router = useRouter();
+
+  const [dataSignIn, setDataSignIn] = useState(initialState);
+
+  function saveState(event) {
+    // initialState[event.target.name] = event.target.value;
+    setDataSignIn({
+      ...dataSignIn,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  async function createUser(event) {
+    event.preventDefault();
+    const url = "https://api.chaan.site/organizer/signup";
+    const options = {
+      method: "POST",
+      body: JSON.stringify(dataSignIn),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    await fetch(url, options)
+      .then((res) => res.json())
+      .then((response) => {
+        /* console.log(response.data.organizer._id);
+        localStorage.setItem(
+          "idUser",
+          JSON.stringify(response.data.organizer._id)
+        );*/
+        router.push("/login-user");
+      });
+  }
+
+  return (
+    <div className={styles.containerLoginAndImg}>
+      <div className={styles.containerLogin}>
+        <div className={styles.contLogin}>
+          <h2 className={styles.titleLogin}>Agrega a tus Invitados</h2>
+          <div className={styles.lineLogin}></div>
+        </div>
+        <div className={styles.contentLogin}>
+          <div className={styles.cardLogin}>
+            <div>
+              <p className={styles.upInputLogin}>Nombre</p>
+              <input
+                className={styles.inputLogin}
+                type="text"
+                placeholder="Nombre"
+                name="name"
+                onChange={saveState}
+                value={dataSignIn.name}
+              />
+            </div>
+            <div>
+              <p className={styles.upInputLogin}>Numero de entradas</p>
+              <input
+                className={styles.inputLogin}
+                type="text"
+                placeholder="Numero de entradas"
+                name="lastName"
+                onChange={saveState}
+                value={dataSignIn.lastName}
+              />
+            </div>
+            <div>
+              <p className={styles.upInputLogin}>Email</p>
+              <input
+                className={styles.inputLogin}
+                type="text"
+                placeholder="Email"
+                name="email"
+                onChange={saveState}
+                value={dataSignIn.email}
+              />
+            </div>
+          </div>
+          <button className={styles.btnL} onClick={createUser}>
+            Agregar
+          </button>
+        </div>
+      </div>
+      <div className={styles.contImgLoginW}>
+        <img className={styles.imgLoginW} src="/inviteeAdd.jpg" alt="" />
+      </div>
+    </div>
+  );
+}
---
+++ pages/create-invitees/[id].js
@@ -0,0 +1,13 @@
+import NavbarUser from "../../components/NavbarUser/NavbarUser";
+import Footer from "../../components/Footer/Footer";
+import CreateInvitees from "../../components/CreateInvitees/CreateInvitees";
+
+export default function About() {
+  return (
+    <div>
+      <NavbarUser> </NavbarUser>
+      <CreateInvitees></CreateInvitees>
+      <Footer></Footer>
+    </div>
+  );
+}
