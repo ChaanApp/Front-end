df1acec978c7e0aff167c7d82b50c650ca0a0695
rgarza237 <roberto.garzah@udem.edu>
2022-05-10 00:53:56 -0500

    create invitee interface

---
+++ pages/createInvitees.js
@@ -0,0 +1,13 @@
+import NavbarLogin from "../components/NavbarLogin/NavbarLogin";
+import Footer from "../components/Footer/Footer";
+import CreateInvitees from "../components/CreateInvitees/CreateInvitees";
+
+export default function About() {
+  return (
+    <div>
+      <NavbarLogin> </NavbarLogin>
+      <CreateInvitees></CreateInvitees>
+      <Footer></Footer>
+    </div>
+  );
+}
