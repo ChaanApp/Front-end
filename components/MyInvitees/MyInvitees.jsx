4301ca59144e0f061da456277a84ee106526c378
Ruben <rub.somuano.g2@gmail.com>
2022-05-12 00:52:42 -0500

    tabla de invitados

--- components/MyInvitees/MyInvitees.jsx
+++ components/MyInvitees/MyInvitees.jsx
@@ -1,41 +1,55 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./myInvitees.module.scss";
 import { DataGrid } from "@mui/x-data-grid";
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 130 },
-  { field: 'lastName', headerName: 'Last name', width: 130 },
+  { field: "id", headerName: "ID", width: 70 },
+  { field: "firstName", headerName: "Nombre", width: 130 },
+
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
+    field: "email",
+    headerName: "Email",
+    type: "string",
     width: 90,
   },
   {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: (params) =>
-      `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    field: "tikets",
+    headerName: "Tikets",
+    type: "number",
+    width: 90,
   },
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+const rows = [{ id: 1, firstName: "", email: "", tikets: 0 }];
 
-export default function MyInvitees() {
+export default function MyInvitees(props) {
+  const [dataInvitees, setDataInvitees] = useState(rows);
+
+  useEffect(() => {
+    if (props.event !== undefined) {
+      const arrayInvitees = props.event.invitees.map((invitee, i) => {
+        return {
+          id: i + 1,
+          firstName: invitee.nameInvitee,
+          email: invitee.emailInvitee,
+          tikets: invitee.asignedTicketsInvitee,
+        };
+
+        /*   setDataInvitees([
+          ...dataInvitees,
+          {
+            id: i + 1,
+            firstName: invitee.nameInvitee,
+            email: invitee.emailInvitee,
+            tikets: invitee.asignedTicketsInvitee,
+          },
+        ]);
+        console.log(dataInvitees);*/
+      });
+      setDataInvitees(arrayInvitees);
+    }
+  }, [props]);
+
   return (
     <div className={styles.maincontainer}>
       <div className={styles.miniheader}>
@@ -48,9 +62,9 @@ export default function MyInvitees() {
         <h4>Invitation</h4>
         <h4> Invitees </h4>
       </div>
-      <div style={{ height: 600, width: '100%' }}>
+      <div style={{ height: 600, width: "100%" }}>
         <DataGrid
-          rows={rows}
+          rows={dataInvitees}
           columns={columns}
           pageSize={10}
           rowsPerPageOptions={[10]}
